e4c9aa77a68cdd7c5e15d21571856bbad2981483
malware <rajoriyamrutyunjay@gmail.com>
2021-01-28 01:44:32 +0530

    A race around condition seem to exist. Sometimes ArrayList is not yet updated and it is called before, in repainting
    more testing is needed

--- experimental/DrawPanel.java
+++ experimental/DrawPanel.java
@@ -15,6 +15,8 @@ public class DrawPanel extends Panel {
 	example reference;
 	int clicks =0;
 	int drags =-1;
+	int currentdX =-1;
+	int currentdY = -1;
 	
 
 	ArrayList xclickPositions;
@@ -38,14 +40,16 @@ public class DrawPanel extends Panel {
 		this.ydragPositions = new ArrayList();
 		
 		this.Keyvalues = new ArrayList();
+		Keyvalues.add(0, 0);
 		
 		this.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				
 				if (reference.val == true && reference.DrawFlag>=0 && reference.DrawFlag<=9 ) {
-			    if (flag == true) {
+			    if (flag == true && drags>=0) {
 			    	
-					Keyvalues.add(clicks, drags ); 
+			    	
+			//		Keyvalues.add(clicks, drags ); 
 				    System.out.println("lets see key values for each x and each y clicked: "+xclickPositions.get(clicks)+" "+yclickPositions.get(clicks)+" "+Keyvalues.get(clicks));
 			    	clicks++;
 			    }
@@ -69,16 +73,20 @@ public class DrawPanel extends Panel {
 				if(isClickInitialised == true && reference.val == true && reference.DrawFlag >=0 && reference.DrawFlag<=9) {
 				
 			if(e.getX()>=0 &&e.getX()<=581   && e.getY()>=0  &&e.getY()<=292) {	
+
+				currentdX++;
+				currentdY++;
 				
 			    drags++;
 			   xdragPositions.add(drags, e.getX());
 			   ydragPositions.add(drags, e.getY());
-			   
+			   Keyvalues.add(clicks, drags );
 
 	    	 System.out.println("Inside DrawPanel,drag;showing cords and drag count "+xdragPositions.get(drags)+" "+ydragPositions.get(drags)+" "+drags);
 			flag = true;
-		
+			
 			repaint();
+			
 			
 			}
 			
@@ -96,9 +104,53 @@ public class DrawPanel extends Panel {
 		if(this.reference.DrawFlag ==0  && this.reference.val == true) {
 		
 			g.setColor(Color.red);
-		g.drawLine(50, 50, 150, 150);
+		
+			
+			if(Keyvalues.size()>=1) {
+			
+			for(int i=0;i<xclickPositions.size();i++) {
+				for(int j=0;j<(int)Keyvalues.get(i);j++) {
+					
+					g.drawLine((int)xdragPositions.get(j), (int)ydragPositions.get(j) , (int)xdragPositions.get(j), (int)ydragPositions.get(j));
+					
+					
+				}
+			}
+			}
+		
+			System.out.println("inside paint, see coords "+xdragPositions.get(currentdX)+" "+ydragPositions.get(currentdY));
+		
+			
+		//	g.fillOval((int)xdragPositions.get(currentdX), (int)ydragPositions.get(currentdY), 5,5);
+			
 			
 		}
+		
+		
+
+		if(this.reference.DrawFlag ==1  && this.reference.val == true) {
+			
+		
+        if(Keyvalues.size()>=1) {
+				
+				int i =0;
+				int j=0;
+			while(i<xclickPositions.size()) {
+			 while(j<(int)Keyvalues.get(i))	{
+				 
+					g.drawLine((int)xclickPositions.get(i),(int)yclickPositions.get(i),(int)xdragPositions.get(j), (int)ydragPositions.get(j));
+					j++;
+			 }
+			 i++;
+			}
+		}
+        
+   		g.drawLine((int)xclickPositions.get(clicks),(int)yclickPositions.get(clicks),(int)xdragPositions.get(currentdX), (int)ydragPositions.get(currentdY));
+	
+			
+		}
+		
+		
 		
 		
    
